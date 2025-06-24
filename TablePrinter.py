a8d8f2ec54c2c7a7c9ca01031910d244d09ba13c
SREENANDHA SUNIL <sreenandha.a.ss.24.bebt@acharya.ac.in>
2025-06-24 13:27:31 +0530

    Create TablePrinter.py

---
+++ TablePrinter.py
@@ -0,0 +1,9 @@
+name=input("SREENANDHA A.S: ")
+USN=input("1AYBT052: ")
+tableData = [
+    ['apples', 'oranges', 'cherries', 'banana'],
+    ['Alice', 'Bob', 'Carol', 'David'],
+    ['dogs', 'cats', 'moose', 'goose']
+]
+for i in range(len(tableData[0])):
+    print(tableData[0][i], tableData[1][i], tableData[2][i])
