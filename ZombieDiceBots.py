0acbcb4c15fe3c30cb2c5e4989477fcc0bff8ea8
SREENANDHA SUNIL <sreenandha.a.ss.24.bebt@acharya.ac.in>
2025-06-24 13:33:26 +0530

    Create ZombieDiceBots.py

---
+++ ZombieDiceBots.py
@@ -0,0 +1,18 @@
+name=input("SREE NANDHA A.S: ")
+USN=input("1AY24BT052: ")
+import random
+dice = ['brain', 'footsteps', 'shotgun']
+brains = 0
+shotguns = 0
+print("Bot's turn:")
+for _ in range(3):
+    roll = random.choice(dice)
+    print("Rolled:", roll)
+    if roll == 'brain':
+        brains += 1
+    elif roll == 'shotgun':
+        shotguns += 1
+print("Brains collected:", brains)
+print("Shotguns hit:", shotguns)
+if shotguns >= 3:
+    print("Turn over due to shotguns!")
