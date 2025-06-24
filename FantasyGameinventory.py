4f11e050e30806cff685d91a38f5429ec47ad616
SREENANDHA SUNIL <sreenandha.a.ss.24.bebt@acharya.ac.in>
2025-06-24 13:21:26 +0530

    Create FantasyGameinventory.py

---
+++ FantasyGameinventory.py
@@ -0,0 +1,10 @@
+name=input("SREENANDHA A.S: ")
+USN=input("1AY24BT052: ")
+inventory = {
+    'gold coin': 5,
+    'dagger': 1,
+    'rope': 2
+}
+print("Inventory:")
+for item in inventory:
+    print(item,inventory[item])
